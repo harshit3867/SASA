ffb5fad65c5202a8459a5e94aaa3ed097122d973
Harshit Saini <harshitnew310@gmail.com>
2026-01-20 14:34:29 +0530

    Update Myntra_hdr_gsheet.js

--- Myntra_hdr_gsheet.js
+++ Myntra_hdr_gsheet.js
@@ -67,23 +67,24 @@ function loadLiveProjections() {
 
 // we add the new hour percentages data here
 const hourPercentages = [
-  { hour: "6-7",   Sunday: 0.59,  Monday: 0.74,  Tuesday: 1.09,  Wednesday: 0.84,  Thursday: 0.93,  Friday: 1.09,  Saturday: 0.51 },
-  { hour: "7-8",   Sunday: 2.60,  Monday: 2.93,  Tuesday: 3.81,  Wednesday: 3.55,  Thursday: 3.12,  Friday: 3.94,  Saturday: 1.81 },
-  { hour: "8-9",   Sunday: 6.57,  Monday: 6.69,  Tuesday: 8.39,  Wednesday: 7.96,  Thursday: 8.11,  Friday: 7.88,  Saturday: 10.88 },
-  { hour: "9-10",  Sunday: 11.02, Monday: 11.35, Tuesday: 13.94, Wednesday: 13.44, Thursday: 13.48, Friday: 13.51, Saturday: 10.88 },
-  { hour: "10-11", Sunday: 18.11, Monday: 16.81, Tuesday: 20.44, Wednesday: 20.15, Thursday: 21.58, Friday: 19.74, Saturday: 16.98 },
-  { hour: "11-12", Sunday: 25.91, Monday: 24.03, Tuesday: 25.81, Wednesday: 26.66, Thursday: 29.73, Friday: 26.46, Saturday: 24.20 },
-  { hour: "12-1",  Sunday: 34.54, Monday: 31.10, Tuesday: 33.87, Wednesday: 32.85, Thursday: 36.84, Friday: 33.57, Saturday: 32.05 },
-  { hour: "1-2",   Sunday: 42.45, Monday: 37.18, Tuesday: 40.87, Wednesday: 38.63, Thursday: 43.05, Friday: 40.94, Saturday: 39.64 },
-  { hour: "2-3",   Sunday: 49.51, Monday: 44.34, Tuesday: 48.07, Wednesday: 44.64, Thursday: 51.02, Friday: 47.14, Saturday: 46.64 },
-  { hour: "3-4",   Sunday: 56.64, Monday: 51.31, Tuesday: 54.30, Wednesday: 50.99, Thursday: 58.56, Friday: 54.00, Saturday: 53.89 },
-  { hour: "4-5",   Sunday: 64.01, Monday: 57.76, Tuesday: 60.18, Wednesday: 58.48, Thursday: 65.94, Friday: 60.02, Saturday: 61.95 },
-  { hour: "5-6",   Sunday: 71.58, Monday: 65.32, Tuesday: 65.94, Wednesday: 66.58, Thursday: 72.89, Friday: 67.99, Saturday: 68.59 },
-  { hour: "6-7",   Sunday: 79.43, Monday: 72.97, Tuesday: 73.41, Wednesday: 74.25, Thursday: 79.41, Friday: 75.25, Saturday: 76.08 },
-  { hour: "7-8",   Sunday: 86.38, Monday: 80.87, Tuesday: 81.12, Wednesday: 81.05, Thursday: 85.75, Friday: 82.96, Saturday: 83.23 },
-  { hour: "8-9",   Sunday: 90.74, Monday: 87.60, Tuesday: 88.18, Wednesday: 89.10, Thursday: 90.90, Friday: 89.70, Saturday: 89.63 },
-  { hour: "9-10",  Sunday: 96.10, Monday: 95.01, Tuesday: 95.92, Wednesday: 95.15, Thursday: 95.73, Friday: 95.48, Saturday: 95.66 },
-  { hour: "10-11", Sunday: 100.00, Monday: 100.01, Tuesday: 100.02, Wednesday: 100.00, Thursday: 100.02, Friday: 100.00, Saturday: 100.00 }
+  { hour: "6-7",   Sunday: 0,  Monday: 0,  Tuesday: 0,  Wednesday: 0,  Thursday: 0,  Friday: 0,  Saturday: 0 },
+  { hour: "7-8",   Sunday: 0.59,  Monday: 0.74,  Tuesday: 1.09,  Wednesday: 0.84,  Thursday: 0.93,  Friday: 1.09,  Saturday: 0.51 },
+  { hour: "8-9",   Sunday: 2.60,  Monday: 2.93,  Tuesday: 3.81,  Wednesday: 3.55,  Thursday: 3.12,  Friday: 3.94,  Saturday: 1.81 },
+  { hour: "9-10",  Sunday: 6.57,  Monday: 6.69,  Tuesday: 8.39,  Wednesday: 7.96,  Thursday: 8.11,  Friday: 7.88,  Saturday: 10.88 },
+  { hour: "10-11", Sunday: 11.02, Monday: 11.35, Tuesday: 13.94, Wednesday: 13.44, Thursday: 13.48, Friday: 13.51, Saturday: 10.88 },
+  { hour: "11-12", Sunday: 18.11, Monday: 16.81, Tuesday: 20.44, Wednesday: 20.15, Thursday: 21.58, Friday: 19.74, Saturday: 16.98 },
+  { hour: "12-13", Sunday: 25.91, Monday: 24.03, Tuesday: 25.81, Wednesday: 26.66, Thursday: 29.73, Friday: 26.46, Saturday: 24.20 },
+  { hour: "13-14", Sunday: 34.54, Monday: 31.10, Tuesday: 33.87, Wednesday: 32.85, Thursday: 36.84, Friday: 33.57, Saturday: 32.05 },
+  { hour: "14-15", Sunday: 42.45, Monday: 37.18, Tuesday: 40.87, Wednesday: 38.63, Thursday: 43.05, Friday: 40.94, Saturday: 39.64 },
+  { hour: "15-16", Sunday: 49.51, Monday: 44.34, Tuesday: 48.07, Wednesday: 44.64, Thursday: 51.02, Friday: 47.14, Saturday: 46.64 },
+  { hour: "16-17", Sunday: 56.64, Monday: 51.31, Tuesday: 54.30, Wednesday: 50.99, Thursday: 58.56, Friday: 54.00, Saturday: 53.89 },
+  { hour: "17-18", Sunday: 64.01, Monday: 57.76, Tuesday: 60.18, Wednesday: 58.48, Thursday: 65.94, Friday: 60.02, Saturday: 61.95 },
+  { hour: "18-19", Sunday: 71.58, Monday: 65.32, Tuesday: 65.94, Wednesday: 66.58, Thursday: 72.89, Friday: 67.99, Saturday: 68.59 },
+  { hour: "19-20", Sunday: 79.43, Monday: 72.97, Tuesday: 73.41, Wednesday: 74.25, Thursday: 79.41, Friday: 75.25, Saturday: 76.08 },
+  { hour: "20-21", Sunday: 86.38, Monday: 80.87, Tuesday: 81.12, Wednesday: 81.05, Thursday: 85.75, Friday: 82.96, Saturday: 83.23 },
+  { hour: "21-22", Sunday: 90.74, Monday: 87.60, Tuesday: 88.18, Wednesday: 89.10, Thursday: 90.90, Friday: 89.70, Saturday: 89.63 },
+  { hour: "22-23", Sunday: 96.10, Monday: 95.01, Tuesday: 95.92, Wednesday: 95.15, Thursday: 95.73, Friday: 95.48, Saturday: 95.66 },
+  { hour: "23-24", Sunday: 100.0, Monday: 100.0, Tuesday: 100.0, Wednesday: 100.0, Thursday: 100.0, Friday: 100.0, Saturday: 100.0 },
 ];
 
 
@@ -1164,4 +1165,5 @@ function excelDateToJSDate(serial) {
 function formatDate(dt) {
     const pad = n => n < 10 ? '0' + n : n;
     return `${pad(dt.getMonth() + 1)}-${pad(dt.getDate())}-${dt.getFullYear()} ${pad(dt.getHours())}:${pad(dt.getMinutes())}`;
+
 }
